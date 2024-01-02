68e23e07cb874369a4fef47c682f452eabff075a
Arif Nur Rizqi <69084186+arifnurrizqi@users.noreply.github.com>
2024-01-02 21:44:43 +0700

    refactoring code
    and changing LCD 16x2 to I2C Communication

--- incubator_en.ino
+++ incubator_en.ino
@@ -1,14 +1,17 @@
 /*---------------------Incubator Project--------------------------
 -----------------------------------------------------------------*/
 
-#include "DHT.h"
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+#include <Adafruit_Sensor.h>
+#include <DHT.h>
 
-#define DHTPIN 2 // define the pin used for the DHT11 sensor
+#define DHTPIN 7 // define the pin used for the DHT11 sensor
 
 // Determine the type of DHT used (choose one)
-// Currently selected is DHT11
-#define DHTTYPE DHT11 // DHT 11 
-
+// Currently selected is DHT22
+// #define DHTTYPE DHT11 // DHT 11 
+#define DHTTYPE DHT22 // DHT 22 
 
 DHT dht(DHTPIN, DHTTYPE);
 
@@ -17,9 +20,7 @@ DHT dht(DHTPIN, DHTTYPE);
 #define RELAY_1  8   // pin used is pin 8
 #define RELAY_2  9   // pin used is pin 9
 
-
-#include <LiquidCrystal.h>
-LiquidCrystal lcd(A0, A1, A2, A3, A4, A5);
+LiquidCrystal_I2C lcd(0x27, 16, 2);  // Alamat I2C untuk LCD 16x2
 
 #define suhuon 11
 #define suhuoff 12
@@ -28,23 +29,21 @@ unsigned long firsttime = 0;
 unsigned long firsttime2 = 0;
 int relayState = LOW;
 
-void setup() {
-    
-  Serial.begin(9600); 
-  Serial.println("DHTxx test!");
-  dht.begin();
-  
- // Set pin as output.
+void setPin() {
+  // Set pin as output.
   pinMode(RELAY_1, OUTPUT);
   pinMode(RELAY_2, OUTPUT);
   pinMode(suhuon, OUTPUT);
   pinMode(suhuoff, OUTPUT);
-   
- // Initialize relay one as off so that on reset it would be off by default
+}
+
+void defaultPinValue() {
+  // Initialize relay one as off so that on reset it would be off by default
   digitalWrite(RELAY_1, RELAY_OFF);
   digitalWrite(RELAY_2, RELAY_OFF);
+}
 
-  lcd.begin(16, 2);
+void firstLcdDisplay() {
   lcd.setCursor(4,0);
   lcd.print("PROJECT");
   lcd.setCursor(3,1);
@@ -55,15 +54,27 @@ void setup() {
   lcd.setCursor(4,0);
   lcd.print("Made by");
   lcd.setCursor(1,1);
-  lcd.print("Arif Nur Rizqi");
+  lcd.print("ARIF NUR RIZQI");
   delay(5000);
   lcd.clear();
+}
 
-  pinMode(13, OUTPUT);  // turn on the fan using pin 13
+void setup() {
+    
+  Serial.begin(9600); 
+  Serial.println("DHTxx test!");
+  lcd.init();  // Tambahkan ini untuk versi library tertentu
+  lcd.backlight();
+  dht.begin();
+  
+  setPin(); // Set pin as output.
+  
+  defaultPinValue(); // Initialize relay one as off so that on reset it would be off by default
+
+  firstLcdDisplay(); // LCD setup display
 }
 
 void loop(){
-  
   int relaytime= 50;
   int Onrelaytime= 12500; // long time rotate eggs (depending on the rpm of the motor)
   unsigned long Offrelaytime= 10800000; // delay 3 hours rotate eggs
@@ -71,66 +82,51 @@ void loop(){
   unsigned long lasttime= millis();
 
   if(lasttime-firsttime>=relaytime){
-      // Read humidity dan temperature
-      float h = dht.readHumidity();
-      float t = dht.readTemperature();
+    // Read humidity dan temperature
+    float h = dht.readHumidity();
+    float t = dht.readTemperature();
 
-      // Check the reading results, and display if ok
-      if (isnan(t) || isnan(h)) {
-          Serial.println("Failed to read from DHT");
-          return;
-      }
+    // Check the reading results, and display if ok
+    if (isnan(t) || isnan(h)) {
+        Serial.println("Failed to read from DHT");
+        return;
+    }
 
-      if (t<37.77)// ON
-      {
-          digitalWrite(RELAY_1, RELAY_ON);
-          digitalWrite(suhuon, HIGH);
-          digitalWrite(suhuoff, LOW);
-      }
-      else if (t>38.33)//OFF
-      {
-          digitalWrite(RELAY_1, RELAY_OFF);
-          digitalWrite(suhuoff, HIGH); 
-          digitalWrite(suhuon, LOW);
-      }
-      //Fan ON
-      
-          digitalWrite(13, HIGH);
+    if (t<37.77)// ON
+    {
+        digitalWrite(RELAY_1, RELAY_ON);
+        digitalWrite(suhuon, HIGH);
+        digitalWrite(suhuoff, LOW);
+    }
+    else if (t>38.33)//OFF
+    {
+        digitalWrite(RELAY_1, RELAY_OFF);
+        digitalWrite(suhuoff, HIGH); 
+        digitalWrite(suhuon, LOW);
+    }
 
+    lcd.setCursor(0,0);
+    lcd.print("Humi: "); 
+    lcd.print(h);
+    lcd.print(" %      ");
+    lcd.setCursor(0,1);
+    lcd.print("Temp: "); 
+    lcd.print(t);
+    lcd.print(" C      ");
 
-      Serial.print("Humidity: ");
-      Serial.print(h);
-      Serial.print(" %t");
-
-      Serial.print("Temperature: "); 
-      Serial.print(t);
-      Serial.println(" *C");
-
-      lcd.setCursor(0,0);
-      lcd.print("Humi: "); 
-      lcd.print(h);
-      lcd.print(" %      ");
-      lcd.setCursor(0,1);
-      lcd.print("Temp: "); 
-      lcd.print(t);
-      lcd.print(" C      ");
-
-      firsttime= millis();
+    firsttime= millis();
 }
-unsigned long lasttime2= millis();
 
-    if((relayState == HIGH) && (lasttime2 - 
-    firsttime2 >= Offrelaytime))
-    {
-        relayState = LOW; // Turn it off
-        firsttime2 = lasttime2; // Remember the time
-        digitalWrite(RELAY_2, relayState); // Update the actual RELAY
-    }
-    else if ((relayState == LOW) && (lasttime2 - 
-    firsttime2 >= Onrelaytime))
-    {
-        relayState = HIGH; // turn it on
-        firsttime2 = lasttime2; // Remember the time
-        digitalWrite(RELAY_2, relayState);	 // Update the actual RELAY
-    }
+  unsigned long lasttime2= millis();
+
+  if((relayState == HIGH) && (lasttime2 - firsttime2 >= Offrelaytime)) {
+      relayState = LOW; // Turn it off
+      firsttime2 = lasttime2; // Remember the time
+      digitalWrite(RELAY_2, relayState); // Update the actual RELAY
+  }
+  else if ((relayState == LOW) && (lasttime2 - firsttime2 >= Onrelaytime)) {
+      relayState = HIGH; // turn it on
+      firsttime2 = lasttime2; // Remember the time
+      digitalWrite(RELAY_2, relayState);	 // Update the actual RELAY
+  }
 }
